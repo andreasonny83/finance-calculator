c0514340c4f9eb7f83a7b116889d56b8038cc10f
Andrea Sonny <andreasonny83@gmail.com>
2016-05-25 22:30:38 +0100

    preparing v.0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # finance-calculator
 finance-calculator WordPress shortcode
+
+Simple WordPress shortcode for rendering a form and a table.
+
+## Installation
+
+1. Copy `finance-calculator.php` inside your theme folder. Ideally, this file will reside inside a subfolder like: `wp-content/themes/my-theme/components/finance-calculator.php`
+
+2. Open your theme's `function.php` and add the following row at the very bottom of the file:
+
+    ```
+    include_once( 'finance-calculator.php' );
+    ```
+    or
+    ```
+    include_once( 'components/finance-calculator.php' );
+    ```
+
+3. Log inside your WP panel and create a new post.
+
+4. Add the following shortcode in your post/page where you want to render the module
+
+    ```
+    [finance-calculator]
+    ```
+
+## Examples
+
+    [finance-calculator]
+    [finance-calculator finance="33.33"]
+    [finance-calculator finance="10" months="10, 12, 36"]
+
+## Attributes
+
+
+#### finance
+
+type: Int
+
+
+Optional: true
+
+Description: The annual percentage rate (eg. "10")
+
+
+
+#### months
+
+type: String
+
+Optional: true
+
+Description: The number of months to render in the table. (eg. "6, 12, 24, 36")
---
+++ finance-calculator.php
@@ -0,0 +1,82 @@
+<?php
+function render_form() {
+  $value = isset($_GET['amount']) ? $_GET['amount'] : null;
+
+  return '<form name="finance_calculator finance_calculator__form" method="get" action="' .
+              htmlspecialchars($_SERVER['REQUEST_URI']) . '">' .
+            '<h2>Finance Calculator</h2>' .
+            '<p><label>Please enter the treatment cost : £' .
+              '<input name="amount" type="number" id="cost" max="25000" step="100" ' .
+              'placeholder="Treatment Cost" min="600"  value="' . $value . '">' .
+              '</label>' .
+              '<input type="submit" value="Calculate">' .
+            '</p>' .
+          '</form>';
+}
+
+function render_table($amount, $months = array(6, 12), $finance = 0) {
+  $total = $amount / 100 * ($finance + 100);
+
+  $output = '' .
+      '<table class="finance_calculator">' .
+        '<tbody>' .
+          '<tr><td colspan="2"><h3 class="finance_calculator finance_calculator__title">' .
+            '<strong>' . $finance . '% Finance</strong>' .
+          '</h3></td></tr>' .
+          '<tr><td colspan="2" class="finance_calculator finance_calculator__no-deposit">' .
+            '<strong>No Deposit</strong>' .
+          '</td></tr>';
+
+  foreach ($months as $month) {
+    $output .= $month === 1 ?
+        '<tr><td><strong>' . $month . ' Month</strong></td>' :
+        '<tr><td><strong>' . $month . ' Months</strong></td>';
+
+    $output .= '<td>£' . round($total / $month, 2, PHP_ROUND_HALF_DOWN) . '</td></tr>';
+  }
+
+  return $output .= '</tbody></table>';
+}
+
+/**
+ * [finance_calculator]
+ * Render a finance calculator table
+ *
+ * example.
+ * [finance-calculator finance="10" months="10, 12, 36"]
+ *
+ * @param  [int] finance    The annual percentage rate.
+ * @param  [string] months  Optional. The number of months to render
+ *                          in the table. (eg. "6, 12, 24, 36")
+ */
+function finance_calculator( $atts ) {
+  $attributes = shortcode_atts( array(
+                'finance' => 'finance',
+                'months'  => 'months',
+            ), $atts );
+  $amount = isset($_GET['amount']) ? $_GET['amount'] : null;
+  $monts = array();
+
+  $no_whitespaces = preg_replace( '/\s*,\s*/', ',', filter_var( $attributes['months'], FILTER_SANITIZE_STRING ) );
+  $months_array = explode( ',', $no_whitespaces );
+
+  if (!$months_array[0]) {
+    $months_array = [24, 36, 48, 60];
+  }
+
+  $output = render_form();
+
+  if (!$amount || $amount === '') {
+    return $output;
+  }
+
+  $output .= render_table($amount, [6, 10, 12]);
+
+  if ($attributes['finance'] && $attributes['finance'] !== null) {
+    $output .= render_table($amount, $months_array, $attributes['finance']);
+  }
+
+  return $output;
+}
+
+add_shortcode( 'finance-calculator', 'finance_calculator' );
