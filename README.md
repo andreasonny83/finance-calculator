9f34d876e1d3bba841b2e9e324c1b621347660fa
Andrea Zornada <a.zornada@resilientplc.com>
2016-08-03 09:07:18 +0100

    Deposit dropdown

--- README.md
+++ README.md
@@ -1,40 +1,48 @@
 # finance-calculator
-finance-calculator WordPress shortcode
+
+> finance-calculator WordPress shortcode
 
 Simple WordPress shortcode for rendering a form and a table.
 
 ## Installation
 
-1. Copy `finance-calculator.php` inside your theme folder. Ideally, this file will reside inside a subfolder like: `wp-content/themes/my-theme/components/finance-calculator.php`
+*   Copy `finance-calculator.php` inside your theme folder. Ideally,
+    this file will reside inside a subfolder like:
+    `wp-content/themes/my-theme/components/finance-calculator.php`
 
-2. Open your theme's `function.php` and add the following row at the very bottom of the file:
+*   Open your theme's `function.php` and add the following row at the
+    very bottom of the file:
 
-    ```
-    include_once( 'finance-calculator.php' );
-    ```
-    or
-    ```
-    include_once( 'components/finance-calculator.php' );
-    ```
+```php
+include_once( 'finance-calculator.php' );
+```
 
-3. Log inside your WP panel and create a new post.
+or
 
-4. Add the following shortcode in your post/page where you want to render the module
+```php
+include_once( 'components/finance-calculator.php' );
+```
 
-    ```
-    [finance-calculator]
-    ```
+*   Log inside your WP panel and create a new post.
+
+*   Add the following shortcode in your post/page where you want
+    to render the module
+
+```php
+[finance-calculator]
+```
 
 ## Examples
 
-    [finance-calculator]
-    [finance-calculator finance="33.33"]
-    [finance-calculator finance="10" months="10, 12, 36"]
+```html
+[finance-calculator]
+[finance-calculator finance="33.33"]
+[finance-calculator finance="10" months="10, 12, 36"]
+```
 
 ## Attributes
 
-
-#### finance
+### finance
 
 type: Int
 
@@ -44,8 +52,7 @@ Default: 10
 
 Description: The annual percentage rate (eg. "10")
 
-
-#### months
+### months
 
 type: String
 
@@ -55,14 +62,63 @@ Default: "24, 36, 48, 60"
 
 Description: The number of months to render in the table. (eg. "6, 12, 24, 36")
 
+## Customization
 
-## Style it up!
+### Minimum treatment cost value
 
-This shortcode comes with no styles. However the HTML elements are already provided with all the classes you might need in order to create a fancy style.
-The following example show how to style the component up using SASS and will produce the following result:
-<img src="http://i.imgur.com/IeKO5iM.png" title="source: imgur.com" />
+You may want to change the minimum value for the treatment cost which is
+currently hardcoded inside the `finance-calculator.php` file.
+To do that, we left a comment in the code to let you easily replace or delete
+that value.
 
+Open your `finance-calculator.php` file, then reach this section of the code:
+
+```php
+// Replace the minimum, maximum, step and placeholder value as you prefer
+
+'min="600" max="25000" step="100" placeholder="Treatment Cost" ' .
+
+// -- //
 ```
+
+Then, simply replace the values with your custom values.
+
+### Deposit values
+
+You may also want to be in control of the preselected deposit values present
+in the dropdown menu.
+Again, you can easily change them from the `finance-calculator.php` file.
+Open the file and reach the following lines of code:
+
+```php
+// Replace the following values with your preferred deposit values
+
+'<option value="0" selected>No Deposit</option>' .
+'<option value="50">50</option>' .
+'<option value="100">100</option>' .
+'<option value="200">200</option>' .
+'<option value="300">300</option>' .
+'<option value="400">400</option>' .
+'<option value="500">500</option>' .
+
+// -- //
+```
+
+Then simply replace the option value and the correspondent text.
+You can also add or remove options to show in the select dropdown.
+
+## Style it up
+
+This shortcode comes with no styles. However the HTML elements are already
+provided with all the classes you might need in order to create a fancy style.
+The following example show how to style the component up using SASS and will
+produce the following result:
+
+```html
+<img src="http://i.imgur.com/IeKO5iM.png" title="source: imgur.com" />
+```
+
+```css
 .finance_calculator {
 
   &.finance_calculator__wrap {
--- finance-calculator.php
+++ finance-calculator.php
@@ -2,38 +2,75 @@
 function render_form() {
   $value = isset($_GET['amount']) ? $_GET['amount'] : null;
 
-  return '<div class="finance_calculator finance_calculator__wrap" id="finance">' .
+  // The Javascript code responsible for updating the values in real time
+  $js = "<script>" .
+        "function _updateCalc(value, target) {" .
+          "var tableID = '.finance_calculator__table--' + target;" .
+          "var amount = document.querySelector('.finance_calculator .finance_calculator__form input').value;" .
+          "var rows = document.querySelectorAll(tableID + ' .finance_calculator__table__row');" .
+          "rows.forEach(function(item) {" .
+            "var months = item.getAttribute('data-months');" .
+            "var rows = item.querySelectorAll('td');" .
+            "var result = (amount - value) / months;" .
+            "rows[1].innerHTML = '£' + result.toFixed(2);" .
+          "});" .
+        "}" .
+        "</script>";
+
+  return $js .
+         '<div class="finance_calculator finance_calculator__wrap" id="finance">' .
             '<form name="finance_calculator" class="finance_calculator finance_calculator__form" method="get" action="' .
                 htmlspecialchars($_SERVER['REQUEST_URI'] . '#finance') . '">' .
               '<h2>Finance Calculator</h2>' .
               '<p><label>Please enter the treatment cost : £' .
-                '<input name="amount" type="number" max="25000" step="100" ' .
-                'placeholder="Treatment Cost" min="600"  value="' . $value . '" />' .
+                '<input name="amount" type="number" ' .
+
+                // Replace the minimum, maximum, step and placeholder value as you prefer
+
+                'min="600" max="25000" step="100" placeholder="Treatment Cost" ' .
+
+                // -- //
+
+                ' value="' . $value . '" />' .
                 '</label>' .
                 '<input type="submit" value="Calculate" />' .
               '</p>' .
           '</form></div>';
 }
 
-function render_table($amount, $months = array(6, 10, 12), $finance = 0) {
+function render_table($tableID, $amount, $months = array(6, 10, 12), $finance = 0) {
   $total =  $amount > 0 ?
             $amount / 100 * ($finance + 100) :
             0;
 
   $output = '' .
-      '<table class="finance_calculator finance_calculator__table">' .
+      '<table class="finance_calculator finance_calculator__table finance_calculator__table--' . $tableID . '">' .
         '<tbody>' .
           '<tr><td colspan="2" class="finance_calculator finance_calculator__table__title"><h3>' .
             '<strong>' . $finance . '% Finance</strong>' .
           '</h3></td></tr>' .
           '<tr><td colspan="2" class="finance_calculator finance_calculator__table__title finance_calculator__table__title--no-deposit">' .
-            '<strong>No Deposit</strong>' .
+            '<select class="finance_calculator__deposit" onchange="_updateCalc(this.value, ' . $tableID . ')">' .
+
+              // Replace the following values with your preferred deposit values
+
+              '<option value="0" selected>No Deposit</option>' .
+              '<option value="50">50</option>' .
+              '<option value="100">100</option>' .
+              '<option value="200">200</option>' .
+              '<option value="300">300</option>' .
+              '<option value="400">400</option>' .
+              '<option value="500">500</option>' .
+
+              // -- //
+
+            '</select>' .
           '</td></tr>';
 
   foreach ($months as $month) {
     $output .= $month === 1 ?
-        '<tr class="finance_calculator finance_calculator__table__row"><td><strong>' . $month . ' Month</strong></td>' :
-        '<tr class="finance_calculator finance_calculator__table__row"><td><strong>' . $month . ' Months</strong></td>';
+        '<tr class="finance_calculator finance_calculator__table__row" data-months="' . $month . '"><td><strong>' . $month . ' Month</strong></td>' :
+        '<tr class="finance_calculator finance_calculator__table__row" data-months="' . $month . '"><td><strong>' . $month . ' Months</strong></td>';
 
     $output .= '<td>£' . round($total / $month, 2, PHP_ROUND_HALF_DOWN) . '</td></tr>';
   }
@@ -73,8 +110,8 @@ function finance_calculator( $atts ) {
   }
 
   $output  = render_form();
-  $output .= render_table($amount, [6, 10, 12]);
-  $output .= render_table($amount, $months_array, $finance);
+  $output .= render_table(0, $amount, [6, 10, 12]);
+  $output .= render_table(1, $amount, $months_array, $finance);
 
   return $output;
 }
